734a80bc3a91a96da42b34b2a0aef98ad8527bc1
Mozey <inedumozey@gmail.com>
2022-08-01 17:36:21 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function index() {
   return (
     <div style={{display:"flex", justify content:"center", alignItems:"center", width:"100vw" height:"100vh"}}>
        <h1 style={{color:"#c20", textAlign:"center"}}>Maintenance On Going</h1>
-       <h3 style={{textAlign:"center"}}>Coming back soon...</h3>
+       <h3 style={{textAlign: "center"}}>Coming back soon...</h3>
     <div/>
   )
 }
