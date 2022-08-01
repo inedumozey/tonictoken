35ce61dda1cc73d35e70843e70a68d507ed46449
Mozey <inedumozey@gmail.com>
2022-08-01 18:04:34 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -31,15 +31,15 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-       <!-- <Head>
-            <title>Tonic Token</title>
+       <Head>
+            <!--<title>Tonic Token</title>-->
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link href="https://fonts.googleapis.com/css2?family=Edu+VIC+WA+NT+Beginner:wght@700&family=Libre+Baskerville:ital,wght@0,700;1,400&family=Open+Sans:wght@600&family=Staatliches&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
           <NextScript />
-        </body> -->
+        </body> 
       </Html>
     )
   }
