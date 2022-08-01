8cc20b773e80de4f02f39bdfc76aca2836c256de
Mozey <inedumozey@gmail.com>
2022-08-01 17:39:31 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import Index from "../components/home/Index"
 export default function index() {
   
   return (
-    <div style={{display:"flex", justify content:"center", alignItems:"center", width:"100vw" height:"100vh"}}>
+    <div style={{display:"flex", justifyContent:"center", alignItems:"center", width:"100vw" height:"100vh"}}>
        <h1 style={{color:"#c20", textAlign:"center"}}>Maintenance On Going</h1>
        <h3 style={{textAlign: "center"}}>Coming back soon...</h3>
-    <div/>
+    </div>
   )
 }
