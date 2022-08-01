383fde2e97e60314f788d3f4c219bcd29c9d9cbc
Mozey <inedumozey@gmail.com>
2022-08-01 17:45:42 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,12 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <GlobalStyles />
-      <Layout>
+      /* <GlobalStyles />
+     <Layout>
         <Component {...pageProps} />
-      </Layout>
+      </Layout>*/
+
+     <Component {...pageProps} />
       
     </>
     
